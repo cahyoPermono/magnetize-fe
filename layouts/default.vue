187ddc88164ad74ce12bd0d281fcf5ec60e3bde2
Dwi Suci Amelia <dwi.amelia@imaniprima.com>
2022-11-28 08:47:15 +0700

    :zap: mengganti link home to dashboard
    mengganti nuxt link di home to dashboard

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="topBar px-4 py-3 shadow-6">
-    <a href="/"><img src="~/assets/magnetize-logo.png" alt="Logo" style="height: 40px" /></a>
+    <a href="/dashboard"><img src="~/assets/magnetize-logo.png" alt="Logo" style="height: 40px" /></a>
     <div style="float: right">
       <Button icon="pi pi-sign-out" class="p-button-text p-button-plain" @click="signout" v-if="isLoggedIn" />
       <Button @click="visibleLeft = true" icon="pi pi-bars" class="p-button-text p-button-plain sidebarTrig" />
@@ -10,7 +10,7 @@
         <p>Home</p>
       </strong>
       <div class="ml-3 mb-3">
-        <NuxtLink to="/">
+        <NuxtLink to="/dashboard">
           <Button icon="pi pi-home" class="p-button-text p-button-plain" label="Home" @click="visibleLeft = false" />
         </NuxtLink>
       </div>
