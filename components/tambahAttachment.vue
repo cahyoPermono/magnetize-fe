687be967c6525c853e1d4466d26b24034bf7281d
zettravos321 <sidna.zen@imaniprima.com>
2022-12-08 14:29:30 +0700

    :art: add notification
    notification on add note and attacchment

--- components/tambahAttachment.vue
+++ components/tambahAttachment.vue
@@ -1,68 +1,73 @@
 <template>
-  <Button label="tambah attachment" icon="pi pi-plus" class="p-button-sm p-button-success mt-3"
-    @click="openModalAttachment" />
-  <Dialog v-model:visible="displayModalAttachment" header="Tambah Attachment"
-    :breakpoints="{ '960px': '75vw', '640px': '90vw' }" :style="{ width: '40vw' }" :modal="true">
-    <div class="card p-3">
-      <div class="mt-2">
-        <label for="attachment"><small>Nama Attachment</small><span style="color: red">*</span></label>
-        <Field class="form-control" name="attachment" v-model="newAttachment.attachment" />
+  <div>
+    <Toast />
+    <Button label="tambah attachment" icon="pi pi-plus" class="p-button-sm p-button-success mt-3"
+      @click="openModalAttachment" />
+    <Dialog v-model:visible="displayModalAttachment" header="Tambah Attachment"
+      :breakpoints="{ '960px': '75vw', '640px': '90vw' }" :style="{ width: '40vw' }" :modal="true">
+      <div class="card p-3">
+        <div class="mt-2">
+          <label for="attachment"><small>Nama Attachment</small><span style="color: red">*</span></label>
+          <Field class="form-control" name="attachment" v-model="newAttachment.attachment" />
+        </div>
+        <div class="mt-2">
+          <label for="formFile"><small>File Attachment</small><span style="color: red">*</span></label>
+          <FileUpload name="demo[]" mode="basic" @upload="onUpload" accept="image/*,.pdf" :maxFileSize="2000000">
+          </FileUpload>
+        </div>
       </div>
-      <div class="mt-2">
-        <label for="formFile"><small>File Attachment</small><span style="color: red">*</span></label>
-        <FileUpload name="demo[]" mode="basic" @upload="onUpload" accept="image/*,.pdf" :maxFileSize="2000000">
-        </FileUpload>
+    </Dialog>
+    <div v-if="attachements === undefined">
+      <div class="card mt-3 p-5" style="display: flex">
+        <div style="text-align: center; opacity: 0.6">
+          <i class="pi pi-info-circle" style="font-size: 2rem"></i>
+          <p>anda belum membuat attachement</p>
+        </div>
       </div>
     </div>
-  </Dialog>
-  <div v-if="attachements === undefined">
-    <div class="card mt-3 p-5" style="display: flex">
-      <div style="text-align: center; opacity: 0.6">
-        <i class="pi pi-info-circle" style="font-size: 2rem"></i>
-        <p>anda belum membuat attachement</p>
-      </div>
+    <div v-else>
+      <DataTable :value="attachements" :paginator="true" :rows="5"
+        paginatorTemplate=" FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+        responsiveLayout="scroll" removableSort>
+        <Column>
+          <template #body="slotProps">
+            <div v-if="isPDF(slotProps.data.type) === true">
+              <div style="opacity: 0.4">
+                <i class="pi pi-file-pdf" style="font-size: 3rem" />
+                <br />
+                <small>PDF File</small>
+              </div>
+            </div>
+            <div v-else>
+              <div style="opacity: 0.4">
+                <i class="pi pi-image" style="font-size: 3rem" /> <br />
+                <small>Image File</small>
+              </div>
+            </div>
+          </template>
+        </Column>
+        <Column field="attachment_name"></Column>
+        <Column>
+          <template #body="slotProps">
+            {{ reverseDate(slotProps.data.createdAt) }}
+          </template>
+        </Column>
+        <Column>
+          <template #body>
+            <Button class="p-button-text p-button-plain" icon="pi pi-external-link" />
+          </template>
+        </Column>
+      </DataTable>
     </div>
-  </div>
-  <div v-else>
-    <DataTable :value="attachements" :paginator="true" :rows="5"
-      paginatorTemplate=" FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-      responsiveLayout="scroll" removableSort>
-      <Column>
-        <template #body="slotProps">
-          <div v-if="isPDF(slotProps.data.type) === true">
-            <div style="opacity: 0.4">
-              <i class="pi pi-file-pdf" style="font-size: 3rem" />
-              <br />
-              <small>PDF File</small>
-            </div>
-          </div>
-          <div v-else>
-            <div style="opacity: 0.4">
-              <i class="pi pi-image" style="font-size: 3rem" /> <br />
-              <small>Image File</small>
-            </div>
-          </div>
-        </template>
-      </Column>
-      <Column field="attachment_name"></Column>
-      <Column>
-        <template #body="slotProps">
-          {{ reverseDate(slotProps.data.createdAt) }}
-        </template>
-      </Column>
-      <Column>
-        <template #body>
-          <Button class="p-button-text p-button-plain" icon="pi pi-external-link" />
-        </template>
-      </Column>
-    </DataTable>
   </div>
 </template>
 
 <script setup>
 import axios from "axios";
 import dateFormat from "dateformat";
+import { useToast } from "primevue/usetoast";
 
+const toast = useToast();
 const route = useRoute();
 const displayModalAttachment = ref(false);
 const newAttachment = reactive({
@@ -105,9 +110,8 @@ async function onUpload(evt) {
       const getAttachments = await axios.get(config.API_BASE_URL + "attachments/dept/" + id);
       attachements.value = getAttachments.data.data;
     };
-    alert("Attachment baru ditambahkan");
+    toast.add({ severity: 'success', summary: 'Create Attachment Success', life: 3000 });
     displayModalAttachment.value = false;
-
   } catch (error) {
     alert(error);
   }
