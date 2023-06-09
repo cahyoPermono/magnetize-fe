4b743649fdbc1c86885727578f50de4050fe75c3
zenimaniprima <sidna.zen@imaniprima.co.id>
2023-06-09 09:16:16 +0700

    :art: add favicon for production
    add favicon for production

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [{
+    name: 'magnetize',
+    port: '3030',
+    script: './.output/server/index.mjs',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
